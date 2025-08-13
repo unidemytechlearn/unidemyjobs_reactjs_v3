5ed8cbc7f4c094f982abdead6db9e973c4c55051
unidemytechlearn <unidemy.techlearn@gmail.com>
2025-08-13 12:08:26 +0530

    Fix useEffect import error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AuthProvider } from './components/AuthProvider';
 import Header from './components/Header';
 import Hero from './components/Hero';
